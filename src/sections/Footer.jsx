bf232f6d1a5f6304109f86e7ddb5151c98f56d45
Abtik Digital <rbhensla1994@gmail.com>
2025-09-03 16:43:14 +0530

    updated Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../assets/Logo/Logo.png";
 function Footer() {
   return (
-    <footer></footer>
-  )
+    <footer className="flex p-16 bg-[#0B1CC8]">
+      <section>
+        <div>
+          <img src={Logo} className="w-20 h-20" />
+        </div>
+        
+      </section>
+      <section></section>
+
+      <section></section>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
